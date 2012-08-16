ec114db2e816cca856be7e044b9b1c2e586b22d0
Joseph Junker <joseph.jnk@gmail.com>
2012-08-16 00:24:26 -0700

    added handling for querystrings

--- examples/dermis.js
+++ examples/dermis.js
@@ -2034,18 +2034,190 @@ var requirejs, require, define;
 
     //Set up with config info.
     req(cfg);
-}(this));(function(){var e,t,n;e={listeners:[],listen:function(e){return n.hash.listeners.push(e)},trigger:function(t){return t==null&&(t=n.hash.value()),t===""&&(t="/"),e.pendingTeardown(function(){var r,i,s,o;e.pendingTeardown=function(e){return e()},o=n.hash.listeners;for(i=0,s=o.length;i<s;i++)r=o[i],r(t)})},value:function(e){return e&&(window.location.hash=e),window.location.hash.replace("#","")}},t={listeners:[],listen:function(e){return n.hash.listeners.push(e)},trigger:function(e){var t,r,i,s;e==null&&(e=n.hash.value()),e===""&&(e="/"),s=n.hash.listeners;for(r=0,i=s.length;r<i;r++)t=s[r],t(e)},value:function(e){return e&&(n.hash.lastHash=e,window.location.hash=e),window.location.hash.replace("#","")},lastHash:null,check:function(){var e;e=n.hash.value(),e!==n.hash.lastHash&&(n.hash.lastHash=e,n.hash.trigger(e)),setTimeout(n.hash.check,100)}},n={init:function(){return n.hash.pendingTeardown=function(e){return e()},n.hash.listen(n.test),n.hash.check?n.hash.check():n.hash.trigger()},routes:{},route:function(e,t,r){var i;i="^"+e+"$",i=i.replace(/([?=,\/])/g,"\\$1").replace(/:([\w\d]+)/g,"([^/]*)"),n.routes[e]={name:e,paramNames:e.match(/:([\w\d]+)/g),pattern:RegExp(i),setup:t,teardown:r?r:function(e){return e()},beforeFilters:[]}},runBeforeFilters:function(e,t,n){var r,i;return i=function(e){return e.length===0?n(null):e.shift()(t,function(t){return t?n(t):i(e)})},r=e.beforeFilters.slice(0),i(r)},test:function(t){var r,i,s,o,u,a,f,l,c,h;s=function(){var e,r,i,s;s=n.routes;for(i in s){e=s[i];if(r=e.pattern.exec(t))return[e,r]}return[null,null]},c=s(),i=c[0],u=c[1];if(!i)return;f={};if(i.paramNames){r=u.slice(1),h=i.paramNames;for(o=0,l=h.length;o<l;o++)a=h[o],f[a.substring(1)]=r[o]}return n.runBeforeFilters(i,f,function(t){if(!t)return e.pendingTeardown=i.teardown,i.setup(f)})},addBeforeFilter:function(e,t){if(!n.routes[e])return;return n.routes[e].beforeFilters.push(t)}},typeof window.onhashchange!="undefined"?(n.hash=e,window.onhashchange=function(){return n.hash.trigger(n.hash.value())}):(n.hash=t,setTimeout(n.hash.check,100)),window.rooter=n}).call(this);(function() {
+}(this));(function() {
+  var hash, hashTimer, rooter,
+    __slice = Array.prototype.slice;
+
+  hash = {
+    listeners: [],
+    listen: function(fn) {
+      return rooter.hash.listeners.push(fn);
+    },
+    trigger: function(newHash) {
+      if (newHash == null) newHash = rooter.hash.value();
+      if (newHash === "") newHash = "/";
+      return hash.pendingTeardown(function() {
+        var fn, _i, _len, _ref;
+        hash.pendingTeardown = function(cb) {
+          return cb();
+        };
+        _ref = rooter.hash.listeners;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          fn = _ref[_i];
+          fn(newHash);
+        }
+      });
+    },
+    value: function(newHash) {
+      if (newHash) window.location.hash = newHash;
+      return window.location.hash.replace('#', '');
+    }
+  };
+
+  hashTimer = {
+    listeners: [],
+    listen: function(fn) {
+      return rooter.hash.listeners.push(fn);
+    },
+    trigger: function(hash) {
+      var fn, _i, _len, _ref;
+      if (hash == null) hash = rooter.hash.value();
+      if (hash === "") hash = "/";
+      _ref = rooter.hash.listeners;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        fn = _ref[_i];
+        fn(hash);
+      }
+    },
+    value: function(newHash) {
+      if (newHash) {
+        rooter.hash.lastHash = newHash;
+        window.location.hash = newHash;
+      }
+      return window.location.hash.replace('#', '');
+    },
+    lastHash: null,
+    check: function() {
+      var currHash;
+      currHash = rooter.hash.value();
+      if (currHash !== rooter.hash.lastHash) {
+        rooter.hash.lastHash = currHash;
+        rooter.hash.trigger(currHash);
+      }
+      setTimeout(rooter.hash.check, 100);
+    }
+  };
+
+  rooter = {
+    init: function() {
+      rooter.hash.pendingTeardown = function(cb) {
+        return cb();
+      };
+      rooter.hash.listen(rooter.test);
+      if (rooter.hash.check) return rooter.hash.check();
+      return rooter.hash.trigger();
+    },
+    routes: {},
+    route: function(expr, setup, teardown) {
+      var pattern;
+      pattern = "^" + expr + "$";
+      pattern = pattern.replace(/([?=,\/])/g, '\\$1').replace(/:([\w\d]+)/g, '([^/]*)').replace(/(\?)\$/g, '\?(.*)');
+      rooter.routes[expr] = {
+        name: expr,
+        paramNames: expr.match(/:([\w\d]+)/g),
+        pattern: new RegExp(pattern),
+        queryString: expr.indexOf('?') === -1 ? false : true,
+        setup: setup,
+        teardown: teardown ? teardown : function(cb) {
+          return cb();
+        },
+        beforeFilters: []
+      };
+    },
+    runBeforeFilters: function(destination, routeInput, cb) {
+      var filters, runFilters;
+      runFilters = function(filterArray) {
+        if (filterArray.length === 0) return cb(null);
+        return filterArray.shift()(routeInput, function(err) {
+          if (err) return cb(err);
+          return runFilters(filterArray);
+        });
+      };
+      filters = destination.beforeFilters.slice(0);
+      return runFilters(filters);
+    },
+    test: function(attemptedHash) {
+      var args, destination, getDestination, idx, junk, matches, name, queryString, routeInput, _i, _len, _ref, _ref2;
+      getDestination = function() {
+        var destination, matches, url, _ref;
+        _ref = rooter.routes;
+        for (url in _ref) {
+          destination = _ref[url];
+          if (matches = destination.pattern.exec(attemptedHash)) {
+            return [destination, matches];
+          }
+        }
+        return [null, null];
+      };
+      _ref = getDestination(), destination = _ref[0], matches = _ref[1];
+      if (!destination) return;
+      routeInput = {};
+      if (destination.paramNames) {
+        args = matches.slice(1);
+        _ref2 = destination.paramNames;
+        for (idx = 0, _len = _ref2.length; idx < _len; idx++) {
+          name = _ref2[idx];
+          routeInput[name.substring(1)] = args[idx];
+        }
+      }
+      if (destination.queryString && attemptedHash.indexOf('?' !== -1)) {
+        junk = 2 <= matches.length ? __slice.call(matches, 0, _i = matches.length - 1) : (_i = 0, []), queryString = matches[_i++];
+      }
+      return rooter.runBeforeFilters(destination, routeInput, function(err) {
+        if (!err) {
+          hash.pendingTeardown = destination.teardown;
+          return destination.setup(routeInput, queryString);
+        }
+      });
+    },
+    addBeforeFilter: function(expr, filter) {
+      if (!rooter.routes[expr]) return;
+      return rooter.routes[expr].beforeFilters.push(filter);
+    }
+  };
+
+  if (typeof window.onhashchange !== 'undefined') {
+    rooter.hash = hash;
+    window.onhashchange = function() {
+      return rooter.hash.trigger(rooter.hash.value());
+    };
+  } else {
+    rooter.hash = hashTimer;
+    setTimeout(rooter.hash.check, 100);
+  }
+
+  window.rooter = rooter;
+
+}).call(this);
+(function() {
   var __slice = Array.prototype.slice;
 
   define("dermis", function(jade, View) {
-    var dermis, getBaseUrl;
-    getBaseUrl = function(url) {
-      var _ref, _ref2;
+    var dermis, getBaseUrl, parseQueryString;
+    getBaseUrl = function(longUrl) {
+      var queryIndex, url, _ref, _ref2;
+      queryIndex = longUrl.indexOf('?');
+      url = (queryIndex === -1 ? longUrl : longUrl.slice(0, queryIndex));
       if (url === '/') {
         return 'index';
       } else {
         return ((_ref = /\/(.*?)\//.exec(url)) != null ? _ref[1] : void 0) || ((_ref2 = /\/(.*)/.exec(url)) != null ? _ref2[1] : void 0);
       }
+    };
+    parseQueryString = function(query) {
+      var decode, match, pl, queryParams, search;
+      queryParams = {};
+      match = void 0;
+      pl = /\+/g;
+      search = /([^&=]+)=?([^&]*)/g;
+      decode = function(s) {
+        return decodeURIComponent(s.replace(pl, " "));
+      };
+      while (match = search.exec(query)) {
+        queryParams[decode(match[1])] = decode(match[2]);
+      }
+      console.log(queryParams);
+      return queryParams;
     };
     dermis = {
       init: function() {
@@ -2058,13 +2230,13 @@ var requirejs, require, define;
         base = getBaseUrl(url);
         if (service == null) service = "routes/" + base;
         if (view == null) view = "templates/" + base;
-        setup = function(rtobj) {
+        setup = function(routeArguments, queryString) {
           console.log("calling route " + base);
           return require([service, view], function(srv, tmpl) {
             if (typeof srv === 'function') {
-              return srv(rtobj, tmpl);
+              return srv(routeArguments, tmpl, parseQueryString(queryString));
             } else {
-              return srv.setup(rtobj, tmpl);
+              return srv.setup(routeArguments, tmpl, parseQueryString(queryString));
             }
           });
         };
--- src/dermis.coffee
+++ src/dermis.coffee
@@ -1,5 +1,20 @@
 define "dermis", (jade, View) ->
-  getBaseUrl = (url) -> (if url is '/' then 'index' else /\/(.*?)\//.exec(url)?[1] or /\/(.*)/.exec(url)?[1])
+  getBaseUrl = (longUrl) ->
+    queryIndex = longUrl.indexOf '?'
+    url = (if queryIndex is -1 then longUrl else longUrl.slice 0, queryIndex)
+    (if url is '/' then 'index' else /\/(.*?)\//.exec(url)?[1] or /\/(.*)/.exec(url)?[1])
+
+  parseQueryString = (query) ->
+    queryParams = {}
+    match = undefined
+    pl = /\+/g # Regex for replacing addition symbol with a space
+    search = /([^&=]+)=?([^&]*)/g
+    decode = (s) ->
+      decodeURIComponent s.replace(pl, " ")
+
+    queryParams[decode(match[1])] = decode(match[2]) while match = search.exec(query)
+    console.log queryParams
+    return queryParams
 
   dermis =
     init: (args...) -> rooter.init args...
@@ -8,13 +23,13 @@ define "dermis", (jade, View) ->
       base = getBaseUrl url
       service ?= "routes/#{base}"
       view ?= "templates/#{base}"
-      setup = (rtobj) ->
+      setup = (routeArguments, queryString) ->
         console.log "calling route #{base}"
         require [service, view], (srv,tmpl) ->
           if typeof srv is 'function'
-            srv rtobj, tmpl
+            srv routeArguments, tmpl, parseQueryString queryString
           else
-            srv.setup rtobj, tmpl
+            srv.setup routeArguments, tmpl, parseQueryString queryString
 
       teardown = (cb) ->
         require [service], (srv) ->
